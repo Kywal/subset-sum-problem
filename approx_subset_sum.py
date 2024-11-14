49c625aaf438a22b7f944e23575ac9f8ebfc999a
r_deborachacon <dtayna.3194@gmail.com>
2024-11-14 20:25:38 -0300

    fix: indices e merge

--- approx_subset_sum.py
+++ approx_subset_sum.py
@@ -4,24 +4,24 @@ from trim import trim
 
 def approx_subset_sum(s: list[int], t: int, e: float):
     n = len(s)
-    lists = [[0]]*n
+    lists = [[0]] + [[] for i in range(n)]
 
     is_less_or_equal_than_t = lambda a : True if a <= t else False
 
-    for i in range(1, n-1):
-        sum_si = lambda a : a + s[i]
+    for i in range(1, n+1): 
+        sum_si = lambda a : a + s[i-1]
 
         lists[i] = merge_lists(
             lists[i-1], list(map(sum_si,lists[i-1])
             )
         )
 
-        lists[i] = trim(lists[i], e/2*n)
+        lists[i] = trim(lists[i], e/(2*n))
         lists[i] = list(
             filter(is_less_or_equal_than_t,lists[i])
         )
 
-    return max(lists[n-1])
+    return max(lists[n]) if lists[n] else 0
 
 
 
