d1abb3700527dfb39e91f6fb3077bf347fec239f
Emanuel Kywal <emanuelkywal@outlook.com>
2024-11-15 14:53:40 -0300

    fix: implementacao correta do merge_lists

--- merge_lists.py
+++ merge_lists.py
@@ -1,4 +1,25 @@
-def merge_lists(l, r):
-    merged_list = l+r
-    merged_list.sort(key=lambda x: x[0])
+import sys
+
+def merge_lists(l: list[(int, list[int])], r: list[(int, list[int])]):
+    merged_list = []
+
+    n = len(l) + len(r)
+
+    p = 0
+    q = 0
+
+    # O endereço após o último endereço de uma das listas vai necessariamente ser acessado
+    # Então, para que um endereço inválido não seja acessado cada um deles recebe "+infinito"
+    # E esse endereço não pode fazer parte da merged_list
+    l.append((sys.maxsize, []))
+    r.append((sys.maxsize, []))
+
+    for i in range(0, n):
+        if l[p][0] <= r[q][0]:
+            merged_list.append(l[p])
+            p += 1
+        else:
+            merged_list.append(r[q])
+            q += 1
+
     return merged_list
---
+++ tests/merge_lists_tests.py
@@ -0,0 +1,6 @@
+from merge_lists import merge_lists
+
+print(merge_lists(
+    [(1, []), (3, []), (5,[])],
+    [(2, []), (4, []), (6, []), (7, [])]
+    ))
