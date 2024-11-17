ecd0e64e6938154085b136a236379c41dcc052b4
Emanuel Kywal <emanuelkywal@outlook.com>
2024-11-17 00:10:04 -0300

    feat: geração aleatória de multiset e solucao otima

---
+++ utils/instance-generator/instance_generator_aux.py
@@ -0,0 +1,40 @@
+import random
+
+
+def instance_generator(s: int , rng: tuple[int, int]):
+    validade_inputs(s, rng)
+    multiset = generate_multiset(s, rng)
+    optimal_sum = generate_optimal_sum(multiset.copy())
+
+    return multiset, optimal_sum
+
+def generate_multiset(s: int, rng: tuple[int, int]):
+    ms = []
+
+    for i in range(0, s):
+        ms.append(random.randint(rng[0], rng[1]))
+
+    return ms
+
+def generate_optimal_sum(ms: list[int]):
+    optimal_config = []
+    optimal_sum = 0
+
+    size_sum = random.randint(1, len(ms))
+    for _ in range(0, size_sum):
+
+        i = random.randint(0, len(ms) - 1)
+        optimal_sum += ms[i]
+        optimal_config.append(ms[i])
+        ms.pop(i)
+
+
+    return optimal_sum, optimal_config
+
+def validade_inputs(s: int, rng: tuple[int, int]):
+
+    if s < 0:
+        raise ValueError("O tamanho do multiset não pode ser negativo!")
+
+    if rng[0] > rng[1]:
+        raise ValueError("O intervalo precisa ser definido de forma crescente! Ex: [1,4]")
