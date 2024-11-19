ea705ef66f2fb8b37ee0808f30532389c2836721
Emanuel Kywal <emanuelkywal@outlook.com>
2024-11-18 20:58:35 -0500

    Revert "feat: instruções de uso"
    This reverts commit 4384c1f689f39951da361300bc4d2cb1e6d337e2.

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import time
 from src.approx_subset_sum import approx_subset_sum
-from src.manager_tests import read_file, write_report, print_readme
+from src.manager_tests import read_file, write_report
 
+s = []
 
-print_readme()
+print("Informe a instância de teste do datatest que deseja executar (ex.: p01, p02...):")
 test_name = input()
 
 s = read_file("datatest/" + test_name + "_w.txt")
 list_t = read_file("datatest/" + test_name + "_c.txt")
 t = list_t[0] if list_t else -1
 
-if s and list_t:
+start_time = time.perf_counter_ns()
+result = approx_subset_sum(s,t,0.4)
+end_time = time.perf_counter_ns()
 
-    start_time = time.perf_counter_ns()
-    result = approx_subset_sum(s,t,0.4)
-    end_time = time.perf_counter_ns()
+data = {
+    "t" : t,
+    "set" : s,
+    "final_sum": str(result[0]),
+    "final_config" :str(result[1]),
+    "duration": end_time - start_time
+}
 
-    data = {
-        "t" : t,
-        "set" : s,
-        "final_sum": str(result[0]),
-        "final_config" :str(result[1]),
-        "duration": end_time - start_time
-    }
-
-    file_name = test_name + ".txt"
-    write_report(file_name, data)
-
-else:
-    print("Não foi possível processar a instância especificada. Verifique se ela se encontra em datatest no formato indicado.")
+file_name = test_name + ".txt"
+write_report(file_name, data)
