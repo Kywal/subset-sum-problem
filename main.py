672b754b4316ee60740fd9f05e5023ba04b13cfc
r_deborachacon <dtayna.3194@gmail.com>
2024-12-17 18:49:03 -0300

    feat: reutilizei a outra interface, mas vou ajeitar

--- main.py
+++ main.py
@@ -2,14 +2,10 @@
 from utils.interface_manager_exact import read_file, write_report, menu, subset_sum_ascii_banner,  print_menu
 from exact_subset_sum.src.exact_subset_sum import all_subset_sums_card, all_subset_sums
 import sys
-sys.setrecursionlimit(30000)  # Aumenta o limite de recursão para 10.000 ou o valor desejado
+sys.setrecursionlimit(30000)
 
 print(subset_sum_ascii_banner())
 print_menu()
 item_menu = input()
 
 menu(item_menu)
-
-#S = {3, 7, 10, 15}
-#u = 20
-#print("AllSubsetSums:", all_subset_sums(S, u))
--- utils/interface_manager_exact.py
+++ utils/interface_manager_exact.py
@@ -84,6 +84,7 @@ def write_report(file_name, data, folder="reports_exact"):
 
 def menu(item_menu):
     while True:  
+        if(item_menu == "1"):
             test_name = input("Informe o nome da instância que deseja executar (ex.: p01, p02...):")
             t,s,list_o = read_file("datatest/" + test_name + ".txt")
             if s and t:
@@ -108,6 +109,37 @@ def menu(item_menu):
             else:
                 print("Não foi possível processar a instância especificada. ")
                 item_menu = input("Digite 0 para voltar ao menu ou 4 para encerrar.")
+        elif (item_menu == '2'):
+            print("Para criar uma nova instância aleatória será necessário informar"
+            f"\n - Quantidade de elementos do conjunto "
+            f"\n - Intervalo no qual os números se encontram (início e fim positivos)")
+            set_len = int(input("Quantidade de elementos do conjunto: "))
+            set_start = int(input("Início do intervalo de números: "))
+            set_end = int(input("Final do intervalo de números: "))
+
+            instance_generator(set_len, (set_start, set_end))
+
+            item_menu = '1'
+        elif (item_menu == '3'):
+            print("-------------------------------------------------------------")
+            print(f"(INSTRUÇÕES) Para adicionar uma instância de teste específica, crie na pasta datatest o arquivo <nome_da_instância>.txt com o seguinte formato:\n",
+            f"<valor da soma>\n",
+            f"<lista de valores do conjunto>\n",
+            f"<lista com valores da solução ótima, se houver, se não for possível fornecer, deixe uma lista vazia []>\n")
+            print("Exemplo de arquivo:\n",
+            f"53\n",
+            f"[15, 22, 14, 26, 32, 9,16, 8]\n",
+            f"[22,14,9,8]\n")
+            print_menu()
+            item_menu = input()
+        elif (item_menu == '4'):
+            break
+        elif (item_menu == '0'):
+            print_menu()
+            item_menu = input()
+        else:
+            print("Entrada inválida. Escolha um item do menu:")
+            item_menu = input()
 
 def subset_sum_ascii_banner():
 
