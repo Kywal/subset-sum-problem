1c79b6c01945ce137422b94b283cd625e421ba5d
r_deborachacon <dtayna.3194@gmail.com>
2025-01-07 16:25:54 -0300

    feat: fitness

--- genetic_subset_sum/genetic_subset_sum.py
+++ genetic_subset_sum/genetic_subset_sum.py
@@ -1,6 +1,19 @@
 import math
 
-def fitness():
+def sum_p(x: list[int], W: list[int]):
+    multiset_size = len(x)
+    result = 0
+    for i in range(multiset_size):
+        result += x[i] * W[i]
+    return result
+
+def fitness(x: list[int], W: list[int], c: int):
+    s = 0
+    px = sum_p(x)
+    if c - px >= 0:
+        s = 1
+    return s * (c - px) + (1 - s) * px
+
 
 def crossover():
 
