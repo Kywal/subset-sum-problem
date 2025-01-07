70819a09a240c4e0ed4e3b8aaf7e6da34a9f3c6d
Emanuel Kywal <emanuelkywal@outlook.com>
2025-01-07 17:58:25 -0300

    feat: mutate

--- genetic_subset_sum/genetic_subset_sum.py
+++ genetic_subset_sum/genetic_subset_sum.py
@@ -31,8 +31,25 @@ def crossover(parent_x: list[int], parent_y: list[int]):
     return child1, child2
 
 
-# def mutate():
-#
+def mutate(parent_x: list[int], parent_y: list[int]):
+    qty_mutation_x = random.randint(1, len(parent_x))
+    qty_mutation_y = random.randint(1, len(parent_y))
+
+    child_x = parent_x
+    child_y = parent_y
+
+    invert = lambda bin_num :  1 if bin_num == 0 else 1
+
+    for _ in range(qty_mutation_x):
+        mutation_index = random.randint(0, len(parent_x))
+        child_x[mutation_index] = invert(child_x[mutation_index])
+
+    for _ in range(qty_mutation_y):
+        mutation_index = random.randint(0, len(parent_y))
+        child_y[mutation_index] = invert(child_y[mutation_index])
+
+    return child_x, child_y
+
 # def selection():
 
 def generate_first_population(multiset_size: int, population_size: int):
