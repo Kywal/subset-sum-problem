236ef51a92129ff007f6bfa405507d404c8e463d
Emanuel Kywal <emanuelkywal@outlook.com>
2025-01-07 16:33:30 -0300

    feat: generate_population and minor fixes on fitness and sum_p

--- genetic_subset_sum/genetic_subset_sum.py
+++ genetic_subset_sum/genetic_subset_sum.py
@@ -1,24 +1,39 @@
 import math
+import random
 
-def sum_p(x: list[int], W: list[int]):
+def sum_p(x: list[int], w: list[int]):
     multiset_size = len(x)
     result = 0
     for i in range(multiset_size):
-        result += x[i] * W[i]
+        result += x[i] * w[i]
     return result
 
-def fitness(x: list[int], W: list[int], c: int):
+def fitness(x: list[int], w: list[int], c: int):
     s = 0
-    px = sum_p(x)
+    px = sum_p(x, w)
     if c - px >= 0:
         s = 1
     return s * (c - px) + (1 - s) * px
 
+# def crossover():
+#
+# def mutate():
+#
+# def selection():
 
-def crossover():
+def generate_population(multiset_size: int, population_size: int):
+    population = set()
 
-def mutate():
+    for _ in range(0, population_size):
+        qty_of_ones_on_dna = random.randint(0, multiset_size)
 
-def selection():
+        for _ in range(0, qty_of_ones_on_dna):
+            one_index = random.randint(0, multiset_size)
 
-def genetic_subset_sum():
+            specimen = [0 for _ in range(multiset_size)]
+            specimen[one_index] = 1
+            population.add(specimen)
+
+    return population
+
+# def genetic_subset_sum():
