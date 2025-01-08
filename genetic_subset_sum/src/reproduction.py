179fb85bd7fc1fd1da24cfe952cda38007642d44
Emanuel Kywal <emanuelkywal@outlook.com>
2025-01-08 10:59:27 -0300

    refact: generate_first_population -> specimen = pair(x, fitness)

--- genetic_subset_sum/src/reproduction.py
+++ genetic_subset_sum/src/reproduction.py
@@ -1,18 +1,20 @@
-from avaliation import difference_degree
+from avaliation import fitness
 import random
 
-def generate_first_population(multiset_size: int, population_size: int):
+def generate_first_population(multiset: list[int], target: int, population_size: int):
+    multiset_size = len(multiset)
     population = set()
 
     for _ in range(0, population_size):
         qty_of_ones_on_dna = random.randint(0, multiset_size)
+        specimen = [[0 for _ in range(multiset_size)], -1]
 
         for _ in range(0, qty_of_ones_on_dna):
             one_index = random.randint(0, multiset_size)
+            specimen[0][one_index] = 1
 
-            specimen = [0 for _ in range(multiset_size)]
-            specimen[one_index] = 1
-            population.add(specimen)
+        specimen[1] = fitness(specimen[0], multiset, target)
+        population.add(specimen)
 
     return population
 
