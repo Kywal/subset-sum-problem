64a47244286df79e2fe38ac5456facfbc33d2f3b
Emanuel Kywal <emanuelkywal@outlook.com>
2025-01-08 12:37:54 -0300

    refact: generate_first_population -> population is a list now

--- genetic_subset_sum/src/reproduction.py
+++ genetic_subset_sum/src/reproduction.py
@@ -4,7 +4,7 @@ import random
 
 def generate_first_population(multiset: list[int], target: int, population_size: int):
     multiset_size = len(multiset)
-    population = set()
+    population = []
 
     for _ in range(0, population_size):
         qty_of_ones_on_dna = random.randint(0, multiset_size)
@@ -15,7 +15,7 @@ def generate_first_population(multiset: list[int], target: int, population_size:
             specimen[0][one_index] = 1
 
         specimen[1] = fitness(specimen[0], multiset, target)
-        population.add(specimen)
+        population.append(specimen)
 
     return population
 
