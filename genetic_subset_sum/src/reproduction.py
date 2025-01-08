3da228dca341f077808bdcf42d9b83a3c7ecd062
Emanuel Kywal <emanuelkywal@outlook.com>
2025-01-08 00:13:23 -0300

    feat: generate_new_population

--- genetic_subset_sum/src/reproduction.py
+++ genetic_subset_sum/src/reproduction.py
@@ -1,4 +1,20 @@
+from avaliation import difference_degree
 import random
+
+def generate_first_population(multiset_size: int, population_size: int):
+    population = set()
+
+    for _ in range(0, population_size):
+        qty_of_ones_on_dna = random.randint(0, multiset_size)
+
+        for _ in range(0, qty_of_ones_on_dna):
+            one_index = random.randint(0, multiset_size)
+
+            specimen = [0 for _ in range(multiset_size)]
+            specimen[one_index] = 1
+            population.add(specimen)
+
+    return population
 
 def crossover(parent_x: list[int], parent_y: list[int]):
     point = random.randint(1, len(parent_x) - 1)
@@ -26,17 +42,26 @@ def mutate(parent_x: list[int], parent_y: list[int]):
 
     return child_x, child_y
 
-def generate_first_population(multiset_size: int, population_size: int):
-    population = set()
+def generate_new_population(parents: list[list[int], list[int], int], setting_diff_degree):
+    new_population = []
+    children_generated = 0
 
-    for _ in range(0, population_size):
-        qty_of_ones_on_dna = random.randint(0, multiset_size)
+    for parent_pair in parents:
 
-        for _ in range(0, qty_of_ones_on_dna):
-            one_index = random.randint(0, multiset_size)
+        parent_x = parent_pair[0]
+        parent_y = parent_pair[1]
+        diff_deg = parent_pair[2]
 
-            specimen = [0 for _ in range(multiset_size)]
-            specimen[one_index] = 1
-            population.add(specimen)
+        if diff_deg > setting_diff_degree:
+            child1, child2 = crossover(parent_x, parent_y)
+            new_population.append(child1)
+            new_population.append(child2)
+            children_generated += 2
+        else:
+            child_x, child_y = mutate(parent_x, parent_y)
+            new_population.append(child_x)
+            new_population.append(child_y)
+            children_generated += 2
 
-    return population
+    return new_population, children_generated
+
