970ca5e7d996a664a4f68420eeeed8d3d5df08ac
r_deborachacon <dtayna.3194@gmail.com>
2025-01-13 22:45:02 -0300

    fix: correcao na geracao da nova populacao e nos > < == de di,Ds

--- genetic_subset_sum/src/genetic_subset_sum.py
+++ genetic_subset_sum/src/genetic_subset_sum.py
@@ -2,13 +2,13 @@ from genetic_subset_sum.src.population_generator import generate_first_populatio
 from genetic_subset_sum.src.avaliation import sum_p, calculate_fitness_of_all_population, select_next_generation_parents
 
 
-def genetic_subset_sum(multiset: list[int], target: int, population_size: int, generations: int, setting_degree_init: int, gamma: float = 0.9):
+def genetic_subset_sum(multiset: list[int], target: int, population_size: int, generations: int, setting_degree_init: int, gamma: float = 0.999):
     population = generate_first_population(multiset, target, population_size)
     setting_diff_degree = setting_degree_init
     best_specimen = []
+    print ("primeira:", len(population))
 
     for generation in range(generations):
-        # sort first population by fitness
         population.sort(key = lambda specimen : specimen[1])
         best_specimen = population[0]
         print(best_specimen)
@@ -19,18 +19,23 @@ def genetic_subset_sum(multiset: list[int], target: int, population_size: int, g
         children_generated = 0
         new_population = []
 
+        cont = 0
         while children_generated < population_size:
+            cont+=1
 
             parents = select_next_generation_parents(population, children_generated)
-            new_population, children_generated = generate_new_population(
+            np, children_generated = generate_new_population(
                 parents,
                 population_size,
                 children_generated,
                 setting_diff_degree
             )
+            new_population = new_population + np
 
             new_population = calculate_fitness_of_all_population(multiset, target, new_population)
             setting_diff_degree = gamma * setting_diff_degree
+
+
 
         population = new_population[:population_size]
         population.sort(key=lambda specimen: specimen[1])
